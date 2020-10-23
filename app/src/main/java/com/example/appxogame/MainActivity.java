0fd8eec39e5063cab87d4581278b31c5f2e60920
ggaallii11 <gali8asaf@gmail.com>
2020-10-23 13:20:47 +0300

    game XO

--- app/src/main/java/com/example/appxogame/MainActivity.java
+++ app/src/main/java/com/example/appxogame/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.appxogame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<Integer> player1 = new ArrayList<>();
+    ArrayList<Integer> player2 = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void processGame(View view) {
+        Toast.makeText(this, "click", Toast.LENGTH_SHORT).show();
+        Button choosesBtn = (Button) view;
     }
 }
